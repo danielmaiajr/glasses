0e48147ad0e6d275598214df8a10a060467f7bf0
Daniel Maia <daniel.maia.jr12@gmail.com>
2021-07-01 16:01:30 -0300

    added product component and dummy data for product

---
+++ src/components/product/data.tsx
@@ -0,0 +1,95 @@
+export interface ProductType {
+	id: string;
+	brand: string;
+	model: string;
+	image: string;
+	sideImage: string;
+	name: string;
+	color: string;
+	description: string;
+	price: number;
+	descountPrice: number;
+	debitPrice: number;
+	inventory: number;
+	measures: string;
+	product_size: string;
+}
+
+export const products: ProductType[] = [
+	{
+		id: '687465',
+		brand: 'HUBO BOSS',
+		model: 'HG 0184',
+		image:
+			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo-boss-hg-0184-preto-wr7-52-1.jpg',
+		sideImage:
+			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo-boss-hg-0184-preto-wr7-52-2.jpg',
+		name: 'Óculos de Grau Hugo Boss',
+		color: 'Preto - WR7/52',
+		description:
+			'BOSS Eyewear é a expressão máxima da estética elegante e do design inovador: novas combinações de materiais de alta tecnologia e detalhes elaborados sublinham o caráter sofisticado de cada coleção. A coleção variada de óculos de sol e óculos de grau combina de forma inteligente, qualidade e funcionalidade, garantindo um ajuste confortável.',
+		price: 1000,
+		descountPrice: 900,
+		debitPrice: 800,
+		inventory: 10,
+		measures: 'M',
+		product_size: '52-16-135'
+	},
+	{
+		id: '9752183',
+		brand: 'HUBO BOSS',
+		model: 'HG 0184',
+		image:
+			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo_hg_0184___tartaruga___0t4_52_0.jpg',
+		sideImage:
+			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo_hg_0184___tartaruga___0t4_52_1.jpg',
+		name: 'Óculos de Grau Hugo Boss Hugo',
+		color: 'Tartaruga - 0T4/52',
+		description:
+			'BOSS Eyewear é a expressão máxima da estética elegante e do design inovador: novas combinações de materiais de alta tecnologia e detalhes elaborados sublinham o caráter sofisticado de cada coleção. A coleção variada de óculos de sol e óculos de grau combina de forma inteligente, qualidade e funcionalidade, garantindo um ajuste confortável.',
+		price: 1000,
+		descountPrice: 900,
+		debitPrice: 800,
+		inventory: 10,
+		measures: 'M',
+		product_size: '52-16-135'
+	},
+	{
+		id: '9752184',
+		brand: 'HUBO BOSS',
+		model: 'HG 0184',
+		image:
+			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo_hg_0184___tartaruga___0t4_52_0.jpg',
+		sideImage:
+			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo_hg_0184___tartaruga___0t4_52_1.jpg',
+		name: 'Óculos de Grau Hugo Boss Hugo',
+		color: 'Tartaruga - 0T4/52',
+		description:
+			'BOSS Eyewear é a expressão máxima da estética elegante e do design inovador: novas combinações de materiais de alta tecnologia e detalhes elaborados sublinham o caráter sofisticado de cada coleção. A coleção variada de óculos de sol e óculos de grau combina de forma inteligente, qualidade e funcionalidade, garantindo um ajuste confortável.',
+		price: 1000,
+		descountPrice: 900,
+		debitPrice: 800,
+		inventory: 10,
+		measures: 'M',
+		product_size: '52-16-135'
+	},
+	{
+		id: '687468',
+		brand: 'HUBO BOSS',
+		model: 'HG 0184',
+		image:
+			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo-boss-hg-0184-preto-wr7-52-1.jpg',
+		sideImage:
+			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo-boss-hg-0184-preto-wr7-52-2.jpg',
+		name: 'Óculos de Grau Hugo Boss',
+		color: 'Preto - WR7/52',
+		description:
+			'BOSS Eyewear é a expressão máxima da estética elegante e do design inovador: novas combinações de materiais de alta tecnologia e detalhes elaborados sublinham o caráter sofisticado de cada coleção. A coleção variada de óculos de sol e óculos de grau combina de forma inteligente, qualidade e funcionalidade, garantindo um ajuste confortável.',
+		price: 1000,
+		descountPrice: 900,
+		debitPrice: 800,
+		inventory: 10,
+		measures: 'M',
+		product_size: '52-16-135'
+	}
+];
---
+++ src/components/product/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+import { ProductType } from './data';
+
+interface PropType {
+	data: ProductType;
+}
+const Product = ({ data }: PropType): JSX.Element => {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.wrapper}>
+			<div>
+				<img src={data.image} width="100%" />
+			</div>
+			<div className={clsx(classes.text, classes.textBold)}>
+				{data.brand} - {data.model}
+			</div>
+			<div className={clsx(classes.text, classes.textSecondary)}>
+				{data.model} - {data.color}
+			</div>
+			<div className={clsx(classes.text, classes.textBold)}>R${data.price},00</div>
+		</div>
+	);
+};
+
+export default Product;
+
+const useStyles = makeStyles((theme: Theme) => ({
+	wrapper: {
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		cursor: 'pointer',
+		padding: 15
+	},
+	text: {
+		letterSpacing: 1,
+		padding: 2.5
+	},
+	textBold: {
+		fontWeight: 'bold'
+	},
+	textSecondary: {
+		color: '#AAA'
+	}
+}));
