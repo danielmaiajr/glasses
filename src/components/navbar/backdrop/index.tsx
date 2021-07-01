5760e34fa6011fc59dfdbf74dcd106ac9d03044e
Daniel Maia <daniel.maia.jr12@gmail.com>
2021-07-01 14:36:41 -0300

    added navBar component

---
+++ src/components/navbar/backdrop/index.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+import { useTransition, useSpring, animated, config } from '@react-spring/web';
+import { useHover } from 'react-use-gesture';
+
+export interface BackdropMenuType {
+	title: String;
+	content: JSX.Element | null;
+}
+
+interface PropTypes {
+	menu: BackdropMenuType[];
+}
+
+const MENU_HEIGHT: number = 40;
+const SPAN_WIDTH: number = 100;
+const BACKDROP_HEIGHT: number = 350;
+
+const BackpropMenu = ({ menu }: PropTypes): JSX.Element => {
+	const [ index, setIndex ] = useState<number>(0);
+	const [ active, setActive ] = useState<boolean>(false);
+
+	const classes = useStyles();
+
+	const activeTransitions = useTransition(active, {
+		from: { height: 0 },
+		enter: { height: BACKDROP_HEIGHT },
+		leave: { height: 0 }
+	});
+
+	const indexTransitions = useTransition(index, {
+		from: { opacity: 0 },
+		enter: { opacity: 1 }
+	});
+
+	const [ { x, scale, color }, api ] = useSpring(() => ({
+		x: 0,
+		scale: 0,
+		color: '#AAA',
+		config: config.stiff
+	}));
+
+	const bind = useHover(({ active }) => {
+		api.start({ scale: active ? 1 : 0, color: active ? '#FFF' : '#AAA' });
+		setActive(false);
+	});
+
+	const bindMenu = useHover(({ active, args: [ i ] }) => {
+		active && setIndex(i);
+		api.start({ x: i * SPAN_WIDTH });
+		setActive(menu[i].content ? true : false);
+	});
+
+	return (
+		<div className={classes.wrapper}>
+			<div className={classes.menuWrapper} {...bind()}>
+				{menu.map((m: BackdropMenuType, i) => (
+					<animated.div
+						key={i}
+						className={classes.menuName}
+						style={i === index ? { color } : {}}
+						{...bindMenu(i)}
+					>
+						{m.title}
+					</animated.div>
+				))}
+
+				<animated.span className={classes.selectedIndex} style={{ x, scale }} />
+
+				{activeTransitions(
+					({ height }, active) =>
+						active && (
+							<animated.div style={{ height }} className={classes.backdropWrapper}>
+								{indexTransitions(({ opacity }) => (
+									<animated.div style={{ opacity }} className={classes.backdropContent}>
+										{menu[index].content}
+									</animated.div>
+								))}
+							</animated.div>
+						)
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default BackpropMenu;
+
+const useStyles = makeStyles((theme: Theme) => ({
+	wrapper: {
+		position: 'relative',
+		display: 'flex',
+		justifyContent: 'center',
+		width: '100%',
+		height: MENU_HEIGHT,
+		fontSize: 10,
+		fontWeight: 'bold',
+		color: '#AAA',
+		textTransform: 'uppercase'
+	},
+	menuWrapper: {
+		display: 'flex'
+	},
+	menuName: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: SPAN_WIDTH,
+		padding: '20px',
+		cursor: 'pointer'
+	},
+	selectedIndex: {
+		position: 'absolute',
+		width: SPAN_WIDTH,
+		bottom: 0,
+		height: 2,
+		backgroundColor: '#d13939'
+	},
+	backdropWrapper: {
+		position: 'fixed',
+		overflow: 'hidden',
+		width: '100%',
+		top: MENU_HEIGHT + 60, //the position is Fixed, so it's in relation with the page
+		left: 0,
+		backgroundColor: '#FFF',
+		color: '#000',
+		boxShadow: '0 1px 3px rgba(0,0,0,.3)',
+		zIndex: 1000
+	},
+	backdropContent: {
+		padding: '20px 40px'
+	}
+}));
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,16 +1,65 @@
+import { useState } from 'react';
+
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core';
 
 import MyTextField from './MyTextField';
 import SearchIcon from '@material-ui/icons/Search';
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+
+import { ReactComponent as CartIcon } from './cart.svg';
+
+import BackpropMenu, { BackdropMenuType } from './backdrop';
+import Drawer from '../drawer';
+
+const Menus: BackdropMenuType[] = [
+	{
+		title: 'title',
+		content: <div>content1</div>
+	},
+	{
+		title: 'title',
+		content: <div>content2</div>
+	},
+	{
+		title: 'title',
+		content: null
+	},
+	{
+		title: 'title',
+		content: null
+	},
+	{
+		title: 'title',
+		content: <div>content4</div>
+	},
+	{
+		title: 'title',
+		content: <div>content5</div>
+	},
+	{
+		title: 'title',
+		content: <div>content6</div>
+	}
+];
 
 const Navbar = (): JSX.Element => {
 	const classes = useStyles();
 
+	const [ showMenu, setShowMenu ] = useState<boolean>(false);
+	const [ showCart, setShowCart ] = useState<boolean>(false);
+	const OnClickMenu: React.MouseEventHandler = (): void => setShowMenu(!showMenu);
+	const OnClickCart: React.MouseEventHandler = (): void => setShowCart(!showCart);
+
 	return (
 		<div className={classes.wrapper}>
 			<Container className={classes.navWrapper}>
-				<div className={classes.logo}>Logo</div>
+				<div className={classes.logoWrapper}>
+					<button className={classes.menuIcon} onClick={OnClickMenu}>
+						<MenuRoundedIcon />
+					</button>
+					<div className={classes.logo}>Logo</div>
+				</div>
 
 				<div className={classes.search}>
 					<MyTextField id="filled-full-width" label="Pesquisar" margin="dense" variant="outlined" fullWidth />
@@ -20,10 +69,23 @@ const Navbar = (): JSX.Element => {
 				</div>
 
 				<div className={classes.navIcons}>
-					<div>Profile</div>
-					<div>Cart</div>
+					<div>Meu Perfil</div>
+					<div className={classes.cartIcon} onClick={OnClickCart}>
+						<CartIcon style={{ width: 32, height: 32 }} />
+					</div>
 				</div>
 			</Container>
+
+			<Container className={classes.backdrop}>
+				<BackpropMenu menu={Menus} />
+			</Container>
+
+			<Drawer show={showMenu} onBackDropClick={OnClickMenu} ltr>
+				Menu
+			</Drawer>
+			<Drawer show={showCart} onBackDropClick={OnClickCart}>
+				CART
+			</Drawer>
 		</div>
 	);
 };
@@ -32,25 +94,39 @@ export default Navbar;
 
 const useStyles = makeStyles((theme: Theme) => ({
 	wrapper: {
-		width: '100%',
-		height: 60,
-		display: 'flex',
-		alignItems: 'center',
 		backgroundColor: '#000',
 		color: '#FFF'
 	},
 	navWrapper: {
+		width: '100%',
+		height: 60,
 		display: 'flex',
 		justifyContent: 'space-between',
 		alignItems: 'center'
 	},
+	logoWrapper: {
+		display: 'flex',
+		alignItems: 'center'
+	},
+	menuIcon: {
+		display: 'none',
+		[theme.breakpoints.down('sm')]: {
+			display: 'flex'
+		}
+	},
 	logo: {
-		fontWeight: 'bold'
+		fontWeight: 'bold',
+		[theme.breakpoints.down('sm')]: {
+			marginLeft: 15
+		}
 	},
 	search: {
 		display: 'flex',
 		alignItems: 'center',
-		width: 450
+		width: 450,
+		[theme.breakpoints.down('sm')]: {
+			display: 'none'
+		}
 	},
 	searchIcon: {
 		width: 55,
@@ -66,6 +142,22 @@ const useStyles = makeStyles((theme: Theme) => ({
 		cursor: 'pointer'
 	},
 	navIcons: {
-		display: 'flex'
-	}
+		display: 'flex',
+		alignItems: 'center'
+	},
+	cartIcon: {
+		display: 'flex',
+		alignItems: 'center',
+		marginLeft: 50,
+		cursor: 'pointer',
+		[theme.breakpoints.down('sm')]: {
+			display: 'none'
+		}
+	},
+	backdrop: {
+		[theme.breakpoints.down('sm')]: {
+			display: 'none'
+		}
+	},
+	bottomNav: {}
 }));
