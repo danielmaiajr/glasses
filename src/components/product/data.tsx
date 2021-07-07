ee53befa381ebecf08cc532e824467d537f4d455
Daniel Maia <daniel.maia.jr12@gmail.com>
2021-07-07 19:18:57 -0300

    color on product data changed to array

--- src/components/product/data.tsx
+++ src/components/product/data.tsx
@@ -4,7 +4,7 @@ export interface ProductType {
 	model: string;
 	image: string[];
 	name: string;
-	color: string;
+	color: string[];
 	description: string;
 	price: number;
 	descountPrice: number;
@@ -24,7 +24,7 @@ export const products: ProductType[] = [
 			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo-boss-hg-0184-preto-wr7-52-2.jpg'
 		],
 		name: 'Óculos de Grau Hugo Boss',
-		color: 'Preto - WR7/52',
+		color: [ 'Preto - WR7/52', 'Tartaruga - 0T4/52' ],
 		description:
 			'BOSS Eyewear é a expressão máxima da estética elegante e do design inovador: novas combinações de materiais de alta tecnologia e detalhes elaborados sublinham o caráter sofisticado de cada coleção. A coleção variada de óculos de sol e óculos de grau combina de forma inteligente, qualidade e funcionalidade, garantindo um ajuste confortável.',
 		price: 1000,
@@ -43,7 +43,7 @@ export const products: ProductType[] = [
 			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo_hg_0184___tartaruga___0t4_52_1.jpg'
 		],
 		name: 'Óculos de Grau Hugo Boss Hugo',
-		color: 'Tartaruga - 0T4/52',
+		color: [ 'Tartaruga - 0T4/52' ],
 		description:
 			'BOSS Eyewear é a expressão máxima da estética elegante e do design inovador: novas combinações de materiais de alta tecnologia e detalhes elaborados sublinham o caráter sofisticado de cada coleção. A coleção variada de óculos de sol e óculos de grau combina de forma inteligente, qualidade e funcionalidade, garantindo um ajuste confortável.',
 		price: 1000,
@@ -62,7 +62,7 @@ export const products: ProductType[] = [
 			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo_hg_0184___tartaruga___0t4_52_1.jpg'
 		],
 		name: 'Óculos de Grau Hugo Boss Hugo',
-		color: 'Tartaruga - 0T4/52',
+		color: [ 'Tartaruga - 0T4/52' ],
 		description:
 			'BOSS Eyewear é a expressão máxima da estética elegante e do design inovador: novas combinações de materiais de alta tecnologia e detalhes elaborados sublinham o caráter sofisticado de cada coleção. A coleção variada de óculos de sol e óculos de grau combina de forma inteligente, qualidade e funcionalidade, garantindo um ajuste confortável.',
 		price: 1000,
@@ -81,7 +81,7 @@ export const products: ProductType[] = [
 			'https://image-tool.eotica.com.br/?d=eo&w={width}&q=75&f=catalog/product/h/u/hugo-boss-hg-0184-preto-wr7-52-2.jpg'
 		],
 		name: 'Óculos de Grau Hugo Boss',
-		color: 'Preto - WR7/52',
+		color: [ 'Preto - WR7/52' ],
 		description:
 			'BOSS Eyewear é a expressão máxima da estética elegante e do design inovador: novas combinações de materiais de alta tecnologia e detalhes elaborados sublinham o caráter sofisticado de cada coleção. A coleção variada de óculos de sol e óculos de grau combina de forma inteligente, qualidade e funcionalidade, garantindo um ajuste confortável.',
 		price: 1000,
