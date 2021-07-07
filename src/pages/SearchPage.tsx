8b97383e807cc838664c386b0a41730b395e6425
Daniel Maia <daniel.maia.jr12@gmail.com>
2021-07-07 19:24:16 -0300

    added seach_bar component to search_page

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 
+import SearchBar from '../components/search_bar';
+import { Container } from '@material-ui/core';
+
 const SearchPage = (): JSX.Element => {
 	const classes = useStyles();
 
-	return <div className={classes.wrapper}>SearchPage</div>;
+	return (
+		<React.Fragment>
+			<Container className={classes.searchWrapper}>
+				<SearchBar />
+			</Container>
+			<Container style={{ marginTop: 10 }}>test</Container>
+		</React.Fragment>
+	);
 };
 
 export default SearchPage;
 
 const useStyles = makeStyles((theme: Theme) => ({
-	wrapper: {}
+	searchWrapper: {
+		width: '100%',
+		height: 60,
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		backgroundColor: '#000',
+		color: '#FFF',
+		[theme.breakpoints.down('sm')]: {
+			boxShadow: '0 1px 3px rgba(0,0,0,.3)'
+		}
+	}
 }));
